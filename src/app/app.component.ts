f0293dae842fc97a12b1787ce35c56be6c603ec2
Hiran95 <hiranmadhushanka95@gmail.com>
2018-06-24 13:45:02 +0530

    end of session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { HotelDataServiceService } from './hotel-data-service.service';
+import { hotel } from './hoteDataModel';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+  colors = ['Red','Blue','Green'];
+  hotels : hotel[];
+  constructor(private hotelService : HotelDataServiceService){
+      this.hotelService.getHotelData()
+      .subscribe((data : hotel[] )=> {
+          this.hotels = data;
+      })
+  }
+
+  ngOnInit(){
+    this.hotelService.getHotelData()
+      .subscribe((data : hotel[] )=> {
+          this.hotels = data;
+      });
+  }  
+  
 }
